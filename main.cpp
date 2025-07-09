89e295ab403141fe82767e8c5e035e0fc2cbde22
Acherontas <145214445+Acherontas@users.noreply.github.com>
2025-07-09 17:39:41 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -15,31 +15,27 @@ int main(int argc,char** argv)
     int preci=0;
     xgen *xg= new xgen();
     cout<<"enter a number \n";
-    cin>>sti;
-    xnmb=stoll(sti);
+    xnmb=stoll(argv[1]);
+    sti+=to_string(xnmb);
     cout<<"enter more than the lenght of number for more 90[X] a number eg 2,3,4 or 0 \n";
-    cin>>prm;
+    prm=stoi(argv[2]);
     cout<<"enter precision \n";
-    cin>>preci;
+    preci=stoi(argv[3]);
     cout<<"for the model enter the first digit \n";
     int x_one;
-    cin>>x_one;
+    x_one=stoi(argv[4]);
     xg->ar[0]=x_one;
     cout<<"for the model enter the second digit \n";
     int y_one;
-    cin>>y_one;
+    y_one=stoi(argv[5]);
     xg->ar[1]=y_one;
     cout<<"for the second model enter the stable is the first always \n";
     xg->size_with(sti);
     lxi=xg->summer(sti);
     cout<<"@ numbers is " <<std::setprecision(preci)<< xnmb <<" with cd of " << lxi <<"\n";
     xg->create_nines_from(prm);
-   // for(int i=0;i<=xg->inh-1;i++){
-   //     cout<<i << " " <<std::setprecision(preci) << xg->internal_holder[i] <<"\n";
-   // }
     cout<<"\n";
     cout<<xg->ar[0] << " " << xg->ar[1] << "model \n";
-
     for(int i=0;i<=xg->inh-1;i++){
        if(xnmb>=xg->internal_holder[i]){xl=xnmb-xg->internal_holder[i];}
        if(xnmb<xg->internal_holder[i]){xl=xg->internal_holder[i]-xnmb;}
@@ -61,14 +57,10 @@ int main(int argc,char** argv)
        cout<<i << " " <<std::setprecision(preci) << xg->internal_holder[i] <<" ";
        cout<<" ---> da rst is " <<std::setprecision(preci) << xl <<" " <<std::setprecision(preci)<< lxi <<"\n";
     }
-
     cout<<"\n";
-    //cout<<"All nines \n";
     cout<<xg->ar[0] << " " << xg->ar[0] << "model \n";
     xg->all_nines_from(prm);
-
     for(int i=0;i<=xg->inh-1;i++){
-
        if(xnmb>=xg->in_h[i]){xl=xnmb-xg->in_h[i];}
        if(xnmb<xg->in_h[i]){xl=xg->in_h[i]-xnmb;}
        if(xl!=0){
